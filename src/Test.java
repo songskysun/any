c34b5ae6a9ee633af3d38457d7c8827131cc3017
Shin_Hyesung <songskysun@gmail.com>
2022-12-10 01:12:18 +0900

    5.6.10 향상된 for문

---
+++ src/Test.java
@@ -0,0 +1,14 @@
+public class Test {
+    public static void main (String[] args) {
+        int[] scores = {79,85,54,32,78};
+        int sum = 0;
+
+        // foreach를 떠오르게 하는 for 문
+        for (int score : scores) {
+            sum += score;
+        }
+
+        System.out.println("합계 : " + sum);
+        System.out.println("평균 : " + sum/scores.length);
+    }
+}
